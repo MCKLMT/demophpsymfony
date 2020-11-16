9d9419648c595f359607f8f250a11650e4a3a907
Mickaël Mottet <mickael.mottet@hotmail.com>
2020-11-16 20:15:25 +0100

    Fix deployment script

--- postbuild.sh
+++ postbuild.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-
 echo '......................'
 
+echo '### Composer Install ###'
+php composer.phar install --no-dev --optimize-autoloader
+
 # Add dependencies
-echo '### php composer.phar dump-env staging ###'
+echo '### Dumping Env ###'
 php composer.phar dump-env staging
 
+echo '### Set Environment variables ###'
 # Configure application
 APP_ENV=prod
 APP_DEBUG=0
 
+echo '### Clear cache ###'
 # Clear cache
 php bin/console cache:clear
+
+echo '......................'
