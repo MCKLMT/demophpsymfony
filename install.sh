a811d22de5be90f42b9f45f2a7acecaa43de6203
Mickaël Mottet <mickael.mottet@hotmail.com>
2020-11-16 18:06:52 +0100

    Fix

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ php composer.phar install --no-dev --optimize-autoloader
 php composer.phar dump-env staging
 
 # Configure application
-APP_ENV=prod
-APP_DEBUG=0
+APP_ENV = prod
+APP_DEBUG = 0
 
 # Clear cache
 php bin/console cache:clear
