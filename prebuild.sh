04f201603a75ec8c9d2d68801b050ada6adc740a
Mickaël Mottet <mickael.mottet@hotmail.com>
2020-11-17 09:51:48 +0100

    Remove unnecessary scripts

--- prebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install Symfony CLI
-curl -sS https://get.symfony.com/cli/installer | bash
-mv /root/.symfony/bin/symfony /usr/local/bin/symfony
-
-# Install Git
-apt-get install git -y
